07163f099719c61fcdd21a4ed11adb1cb381357e
Timeboard <hello@timeboard.me>
2019-05-16 22:53:12 +0530

    changed Smartcloud class to module in docker

--- lib/smartcloud/docker.rb
+++ lib/smartcloud/docker.rb
@@ -1,50 +1,52 @@
 # The main Smartcloud Docker driver
-class Smartcloud::Docker
-	def initialize
-	end
+module Smartcloud
+	class Docker
+		def initialize
+		end
 
-	# Installing Docker!
-	#
-	# Example:
-	#   >> Docker.install
-	#   => Installation Complete
-	#
-	# Arguments:
-	#   none
-	def self.install
-		puts "-----> Installing Docker"
-		system("sudo apt-get update")
-		system("sudo apt-get install apt-transport-https ca-certificates curl software-properties-common")
-		system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
-		system("sudo apt-key fingerprint 0EBFCD88")
-		system("sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"")
-		system("sudo apt-get update")
-		system("sudo apt-get install docker-ce")
-		system("sudo usermod -aG docker $USER")
-		system("docker run --rm hello-world")
-		puts "-----> Installing Docker Compose"
-		system("sudo curl -L --fail https://github.com/docker/compose/releases/download/1.21.2/run.sh -o /usr/local/bin/docker-compose")
-		system("sudo chmod +x /usr/local/bin/docker-compose")
-		system("docker-compose --version")
-		system("sudo curl -L https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose")
-		puts "-----> Installation Complete"
-	end
+		# Installing Docker!
+		#
+		# Example:
+		#   >> Docker.install
+		#   => Installation Complete
+		#
+		# Arguments:
+		#   none
+		def self.install
+			puts "-----> Installing Docker"
+			system("sudo apt-get update")
+			system("sudo apt-get install apt-transport-https ca-certificates curl software-properties-common")
+			system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+			system("sudo apt-key fingerprint 0EBFCD88")
+			system("sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"")
+			system("sudo apt-get update")
+			system("sudo apt-get install docker-ce")
+			system("sudo usermod -aG docker $USER")
+			system("docker run --rm hello-world")
+			puts "-----> Installing Docker Compose"
+			system("sudo curl -L --fail https://github.com/docker/compose/releases/download/1.21.2/run.sh -o /usr/local/bin/docker-compose")
+			system("sudo chmod +x /usr/local/bin/docker-compose")
+			system("docker-compose --version")
+			system("sudo curl -L https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose")
+			puts "-----> Installation Complete"
+		end
 
-	# Uninstalling Docker!
-	#
-	# Example:
-	#   >> Docker.uninstall
-	#   => Uninstallation Complete
-	#
-	# Arguments:
-	#   none
-	def self.uninstall
-		puts "-----> Uninstalling Docker Compose"
-		system("sudo rm /usr/local/bin/docker-compose")
-		puts "-----> Uninstalling Docker"
-		system("sudo apt-get purge docker-ce")
-		system("sudo rm -rf /var/lib/docker")
-		puts "-----> Uninstallation Complete"
-		puts "-----> You must delete any edited configuration files manually."
+		# Uninstalling Docker!
+		#
+		# Example:
+		#   >> Docker.uninstall
+		#   => Uninstallation Complete
+		#
+		# Arguments:
+		#   none
+		def self.uninstall
+			puts "-----> Uninstalling Docker Compose"
+			system("sudo rm /usr/local/bin/docker-compose")
+			puts "-----> Uninstalling Docker"
+			system("sudo apt-get purge docker-ce")
+			system("sudo rm -rf /var/lib/docker")
+			puts "-----> Uninstallation Complete"
+			puts "-----> You must delete any edited configuration files manually."
+		end
 	end
 end
