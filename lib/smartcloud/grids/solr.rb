76393da34614be233f743434f26eecfc547a3181
Timeboard <hello@timeboard.me>
2019-05-17 02:22:37 +0530

    added solr grid base files

--- lib/smartcloud/grids/solr.rb
+++ lib/smartcloud/grids/solr.rb
@@ -6,9 +6,27 @@ module Smartcloud
 			end
 
 			def self.start
+				# echo "-----> Setting up data folder used by solr"
+				# # Give the ownership of solr folder to 8983 for solr to use properly
+				# sudo chown -R 8983:8983 ./solr
+				#
+				# echo "-----> Starting Solr Network"
+				# docker-compose up -d
 			end
 	
 			def self.stop
+				# echo "-----> Stopping Solr Network"
+				# docker-compose down
+			end
+
+			def self.create_core
+				# solr_corename=$1
+				# sudo docker exec -it --user=solr solr solr create_core -c ${solr_corename} -d sunspot
+			end
+
+			def self.destroy_core
+				# solr_corename=$1
+				# sudo docker exec -it --user=solr solr solr delete -c ${solr_corename}
 			end
 		end
 	end
