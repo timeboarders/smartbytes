581d3d0436c96f6fdd6c9ab7410a3a5c03acbef6
Timeboard <hello@timeboard.me>
2019-05-17 01:05:37 +0530

    added commented lines for git grid start and stop methods

--- lib/smartcloud/grids/git.rb
+++ lib/smartcloud/grids/git.rb
@@ -6,9 +6,42 @@ module Smartcloud
 			end
 	
 			def self.start
+				# if ! $(free | awk '/^Swap:/ {exit !$2}'); then
+				# 	echo "-----> Adding Swap for bundler to work properly"
+				# 	sudo swapon -s
+				# 	sudo dd if=/dev/zero of=/swapfile bs=1024 count=256k
+				# 	sudo mkswap /swapfile
+				# 	sudo chmod 0600 /swapfile
+				# 	sudo swapon /swapfile
+				# 	sudo sh -c 'echo "/swapfile       none    swap    sw      0       0" >> /etc/fstab'
+				# 	echo 10 | sudo tee /proc/sys/vm/swappiness
+				# 	echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
+				# 	sudo chown root:root /swapfile
+				# fi
+				#
+				# if [[ "$(sudo docker images -q tbcloud/git-receive 2> /dev/null)" == "" ]]; then
+				# 	echo "-----> Building Image with Git Receive Essentials"
+				# 	docker image build -t tbcloud/git-receive .
+				# fi
+				#
+				# if [[ "$(sudo docker images -q tbcloud/ruby-2.5.1 2> /dev/null)" == "" ]]; then
+				# 	echo "-----> Building Image with Ruby on Rails Essentials"
+				# 	docker image build -t tbcloud/ruby-2.5.1 ./buildpacks/rails-pack/.
+				# fi
+				#
+				# if [[ "$(sudo docker images -q tbcloud/git-receive 2> /dev/null)" != "" && "$(sudo docker images -q tbcloud/ruby-2.5.1 2> /dev/null)" != "" ]]; then
+				# 	echo "-----> Starting Git Network"
+				# 	cp ../.env .env
+				# 	docker-compose up -d
+				# 	rm .env
+				# fi
 			end
 	
 			def self.stop
+				# echo "-----> Stopping Git Network"
+				# cp ../.env .env
+				# docker-compose down
+				# rm .env
 			end
 		end
 	end
