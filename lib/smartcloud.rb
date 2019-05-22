861dacef0a160192b944fb272cebed58250e6b8f
Timeboard <hello@timeboard.me>
2019-05-23 00:35:18 +0530

    smartcloud initialise completed

--- lib/smartcloud.rb
+++ lib/smartcloud.rb
@@ -1,5 +1,12 @@
+require "ostruct"
+require "tempfile"
+
 # The main Smartcloud driver
 module Smartcloud
+	def self.config
+		@@config ||= OpenStruct.new
+	end
+
 	def self.root
 		@@root ||= File.expand_path('../..', __FILE__)
 	end
@@ -8,17 +15,60 @@ module Smartcloud
 		@@user_home ||= File.expand_path('~')
 	end
 
-	def self.domain
-		"yourdomain.com"
+	def self.init
+		# Copy Template for dotsmartcloud
+		unless Dir.exist? "#{Smartcloud.user_home}/.smartcloud"
+			FileUtils.cp_r("#{Smartcloud.root}/lib/smartcloud/templates/dotsmartcloud", "#{Smartcloud.user_home}/.smartcloud")
+		end
+
+		# Setup environment values
+		begin
+			print "Enter your domain without any subdomain (Eg: yourdomain.com): "
+			config_domain = STDIN.gets.chomp
+			raise if config_domain.empty?
+		rescue
+			retry
+		end
+
+		config_git_subdomain = "git"
+		print "Enter subdomain for git grid setup. Eg: If you want gitlive.#{config_domain}, then enter gitlive. [Default: #{config_git_subdomain}]: "
+		subdomain = STDIN.gets.chomp
+		config_git_subdomain = subdomain unless subdomain.empty?
+
+		config_admin_email = "admin@#{config_domain}"
+		print "Enter admin email id. [Default: #{config_admin_email}]: "
+		admin_email = STDIN.gets.chomp
+		config_admin_email = admin_email unless admin_email.empty?
+
+		tempFile = Tempfile.new("#{Smartcloud.user_home}/.smartcloud/config/environmentTemp.rb")
+		File.open("#{Smartcloud.user_home}/.smartcloud/config/environment.rb", "r").each_line do |line|
+			if line =~ /Smartcloud.config.domain/
+				tempFile.puts "Smartcloud.config.domain = \"#{config_domain}\""
+			elsif line =~ /Smartcloud.config.admin_email/
+				tempFile.puts "Smartcloud.config.admin_email = \"#{config_admin_email}\""
+			elsif line =~ /Smartcloud.config.git_subdomain/
+				tempFile.puts "Smartcloud.config.git_subdomain = \"#{config_git_subdomain}\""
+			else
+				tempFile.puts line
+			end
+		end
+		tempFile.close
+		FileUtils.mv(tempFile.path, "#{Smartcloud.user_home}/.smartcloud/config/environment.rb")
+
+		puts "\nIMPORTANT NOTE: Please ensure that the required domain '#{config_domain}' and/or its required subdomains are pointing to this server using DNS Records."
+		puts "\nInitialising Smartcloud ... done"
 	end
 
-	def self.admin_email
-		"admin@yourdomain.com"
+	def self.initialised!
+		unless Dir.exist? "#{Smartcloud.user_home}/.smartcloud"
+			puts "Smartcloud has not been initialized. Please run command 'smartcloud init'"
+			exit
+		end
 	end
+end
 
-	def self.letsencrypt_test
-		false
-	end
+if File.exists?("#{Smartcloud.user_home}/.smartcloud/config/environment.rb")
+	require "#{Smartcloud.user_home}/.smartcloud/config/environment"
 end
 
 require 'smartcloud/docker'
--- lib/smartcloud/templates/dotsmartcloud/config/environment.rb
+++ lib/smartcloud/templates/dotsmartcloud/config/environment.rb
@@ -1,3 +1,13 @@
-DOMAIN_NAME=git.example.com
-ADMIN_EMAIL=admin@example.com
-APPS_ROOT=/path/to/apps/folder
+# Be sure to restart your server when you modify this file.
+
+# naked domain name
+Smartcloud.config.domain = "yourdomain.com"
+
+# system admin email id
+Smartcloud.config.admin_email = "admin@yourdomain.com"
+
+# letsencrypt test boolean
+Smartcloud.config.letsencrypt_test = false
+
+# subdomain to be used for git
+Smartcloud.config.git_subdomain = "git"
