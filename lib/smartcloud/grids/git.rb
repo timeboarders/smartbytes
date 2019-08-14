63b5ca65302cb02ff693c5f1c75458a738d20a89
Timeboard <hello@timeboard.me>
2019-08-14 13:59:30 +0530

    setup git grid

--- lib/smartcloud/grids/git.rb
+++ lib/smartcloud/grids/git.rb
@@ -7,38 +7,11 @@ module Smartcloud
 
 			def self.start
 				if Smartcloud::Docker.running?
-					# Creating swapfile for bundler to work properly
-					unless system("sudo swapon -s | grep -ci '/swapfile'", out: File::NULL)
-						print "-----> Creating swap swapfile ... "
-						system("sudo install -o root -g root -m 0600 /dev/null /swapfile", out: File::NULL)
-						system("sudo dd if=/dev/zero of=/swapfile bs=1k count=2048k", [:out, :err] => File::NULL)
-						system("sudo mkswap /swapfile", out: File::NULL)
-						system("sudo sh -c 'echo \"/swapfile       none    swap    sw      0       0\" >> /etc/fstab'", out: File::NULL)
-						system("echo 10 | sudo tee /proc/sys/vm/swappiness", out: File::NULL)
-						system("sudo sed -i '/^vm.swappiness = /d' /etc/sysctl.conf", out: File::NULL)
-						system("echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf", out: File::NULL)
-						puts "done"
-
-						print "-----> Starting swap swapfile ... "
-						if system("sudo swapon /swapfile", out: File::NULL)
-							puts "done"
-						end
-					end
+					# Creating swapfile
+					# self.create_swapfile
 
 					# Creating images
-					unless system("docker image inspect smartcloud/git-receive", [:out, :err] => File::NULL)
-						print "-----> Creating image smartcloud/git-receive ... "
-						if system("docker image build -t smartcloud/git-receive #{Smartcloud.root}/lib/smartcloud/grids/grid-git", out: File::NULL)
-							puts "done"
-						end
-					end
-
-					unless system("docker image inspect smartcloud/buildpacks/rails", [:out, :err] => File::NULL)
-						print "-----> Creating image smartcloud/buildpacks/rails ... "
-						if system("docker image build -t smartcloud/buildpacks/rails #{Smartcloud.root}/lib/smartcloud/grids/grid-git/buildpacks/rails", out: File::NULL)
-							puts "done"
-						end
-					end
+					self.create_images
 
 					# Creating & Starting containers
 					if system("docker image inspect smartcloud/git-receive", [:out, :err] => File::NULL) && system("docker image inspect smartcloud/buildpacks/rails", [:out, :err] => File::NULL)
@@ -46,12 +19,12 @@ module Smartcloud
 						if system("docker create \
 							--name='git-receive' \
 							--env VIRTUAL_PROTO=fastcgi \
-							--env VIRTUAL_HOST=#{self.virtual_host} \
-							--env LETSENCRYPT_HOST=#{self.letsencrypt_host} \
-							--env LETSENCRYPT_EMAIL=#{self.letsencrypt_email} \
-							--env LETSENCRYPT_TEST=#{self.letsencrypt_test} \
+							--env VIRTUAL_HOST=#{Smartcloud.config.git_host} \
+							--env LETSENCRYPT_HOST=#{Smartcloud.config.git_host} \
+							--env LETSENCRYPT_EMAIL=#{Smartcloud.config.git_admin_email} \
+							--env LETSENCRYPT_TEST=#{Smartcloud.config.git_letsencrypt_test} \
 							--expose='9000' \
-							--volume='#{Smartcloud.user_home}/.smartcloud/apps:/apps' \
+							--volume='#{Smartcloud.config.user_home_path}/.smartcloud/apps:/apps' \
 							--volume='/var/run/docker.sock:/var/run/docker.sock' \
 							--restart='always' \
 							--network='nginx-network' \
@@ -82,52 +55,127 @@ module Smartcloud
 					end
 
 					# Removing images
-					if system("docker image inspect smartcloud/buildpacks/rails", [:out, :err] => File::NULL)
-						print "-----> Removing image smartcloud/buildpacks/rails ... "
-						if system("docker image rm smartcloud/buildpacks/rails", out: File::NULL)
-							puts "done"
-						end
-					end
-
-					if system("docker image inspect smartcloud/git-receive", [:out, :err] => File::NULL)
-						print "-----> Removing image smartcloud/git-receive ... "
-						if system("docker image rm smartcloud/git-receive", out: File::NULL)
-							puts "done"
-						end
-					end
+					# self.destroy_images
 
 					# Removing swapfile
-					if system("sudo swapon -s | grep -ci '/swapfile'", out: File::NULL)
-						print "-----> Stopping swap swapfile ... "
-						if system("sudo swapoff /swapfile", out: File::NULL)
-							system("sudo sed -i '/^vm.swappiness = /d' /etc/sysctl.conf", out: File::NULL)
-							system("echo 60 | sudo tee /proc/sys/vm/swappiness", out: File::NULL)
-							puts "done"
+					# self.destroy_swapfile
+				end
+			end
+			
+			def self.create_swapfile
+				# Creating swapfile for bundler to work properly
+				unless system("sudo swapon -s | grep -ci '/swapfile'", out: File::NULL)
+					print "-----> Creating swap swapfile ... "
+					system("sudo install -o root -g root -m 0600 /dev/null /swapfile", out: File::NULL)
+					system("sudo dd if=/dev/zero of=/swapfile bs=1k count=2048k", [:out, :err] => File::NULL)
+					system("sudo mkswap /swapfile", out: File::NULL)
+					system("sudo sh -c 'echo \"/swapfile       none    swap    sw      0       0\" >> /etc/fstab'", out: File::NULL)
+					system("echo 10 | sudo tee /proc/sys/vm/swappiness", out: File::NULL)
+					system("sudo sed -i '/^vm.swappiness = /d' /etc/sysctl.conf", out: File::NULL)
+					system("echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf", out: File::NULL)
+					puts "done"
 
-							print "-----> Removing swap swapfile ... "
-							system("sudo sed -i '/^\\/swapfile/d' /etc/fstab", out: File::NULL)
-							if system("sudo rm /swapfile", out: File::NULL)
-								puts "done"
-							end
-						end
+					print "-----> Starting swap swapfile ... "
+					if system("sudo swapon /swapfile", out: File::NULL)
+						puts "done"
 					end
 				end
 			end
 
-			def self.virtual_host
-				"#{Smartcloud.config.git_subdomain}.#{Smartcloud.config.domain}"
+			def self.destroy_swapfile
+				if system("sudo swapon -s | grep -ci '/swapfile'", out: File::NULL)
+					print "-----> Stopping swap swapfile ... "
+					if system("sudo swapoff /swapfile", out: File::NULL)
+						system("sudo sed -i '/^vm.swappiness = /d' /etc/sysctl.conf", out: File::NULL)
+						system("echo 60 | sudo tee /proc/sys/vm/swappiness", out: File::NULL)
+						puts "done"
+
+						print "-----> Removing swap swapfile ... "
+						system("sudo sed -i '/^\\/swapfile/d' /etc/fstab", out: File::NULL)
+						if system("sudo rm /swapfile", out: File::NULL)
+							puts "done"
+						end
+					end
+				end
+			end
+			
+			def self.create_images
+				unless system("docker image inspect smartcloud/git-receive", [:out, :err] => File::NULL)
+					print "-----> Creating image smartcloud/git-receive ... "
+					if system("docker image build -t smartcloud/git-receive #{Smartcloud.config.root_path}/lib/smartcloud/grids/grid-git", out: File::NULL)
+						puts "done"
+					end
+				end
+
+				unless system("docker image inspect smartcloud/buildpacks/rails", [:out, :err] => File::NULL)
+					print "-----> Creating image smartcloud/buildpacks/rails ... "
+					if system("docker image build -t smartcloud/buildpacks/rails #{Smartcloud.config.root_path}/lib/smartcloud/grids/grid-git/buildpacks/rails", out: File::NULL)
+						puts "done"
+					end
+				end
+			end
+			
+			def self.destroy_images
+				if system("docker image inspect smartcloud/buildpacks/rails", [:out, :err] => File::NULL)
+					print "-----> Removing image smartcloud/buildpacks/rails ... "
+					if system("docker image rm smartcloud/buildpacks/rails", out: File::NULL)
+						puts "done"
+					end
+				end
+
+				if system("docker image inspect smartcloud/git-receive", [:out, :err] => File::NULL)
+					print "-----> Removing image smartcloud/git-receive ... "
+					if system("docker image rm smartcloud/git-receive", out: File::NULL)
+						puts "done"
+					end
+				end
 			end
 
-			def self.letsencrypt_host
-				self.virtual_host
-			end
-
-			def self.letsencrypt_email
-				Smartcloud.config.admin_email
-			end
-
-			def self.letsencrypt_test
-				Smartcloud.config.letsencrypt_test
+			def self.prereceive(oldrev, newrev, refname)
+				# Verify the user and ensure the user is correct and has access to this repository
+				
+				# # Only run this script for the master branch. You can remove this
+				# # if block if you wish to run it for others as well.
+				# if [ "$refname" == "refs/heads/master" ]; then
+				#
+				# 	echo "-----> Initializing Application"
+				# 	if [ $(git rev-parse --is-bare-repository) = true ]; then
+				# 	    REPOSITORY_BASENAME=$(basename "$PWD")
+				# 		REPOSITORY_BASENAME=${REPOSITORY_BASENAME%.git}
+				# 	else
+				# 	    REPOSITORY_BASENAME=$(basename $(readlink -nf "$PWD"/..))
+				# 	fi
+				# 	REPOSITORY_PATH=/apps/containers/${REPOSITORY_BASENAME}
+				# 	NOW_DATE=$(date +"%Y%m%d%H%M%S")
+				# 	[[ ! -d "$REPOSITORY_PATH/$NOW_DATE" ]] && mkdir -p $REPOSITORY_PATH/$NOW_DATE && git archive "$newrev" | tar -x -C $REPOSITORY_PATH/$NOW_DATE
+				#
+				#
+				# 	if [ -f $REPOSITORY_PATH/$NOW_DATE/bin/rails ]; then
+				# 		echo "-----> Ruby on Rails Application Detected"
+				#
+				# 		if [ ! -f "$REPOSITORY_PATH/$NOW_DATE/Procfile" ]; then
+				# 		echo "-----> Procfile not detected. Please add Procfile and try again."
+				# 			exit 1;
+				# 		fi
+				#
+				# 		if [ ! -f "$REPOSITORY_PATH/env" ]; then
+				# 			echo "-----> Generating Environment Variables File"
+				# 			cat > $REPOSITORY_PATH/env <<- EOF
+				# 				##### Docker
+				# 				VIRTUAL_HOST=$REPOSITORY_BASENAME.$DOMAIN_NAME
+				# 				# LETSENCRYPT_HOST=$REPOSITORY_BASENAME.$DOMAIN_NAME
+				# 				# LETSENCRYPT_EMAIL=admin@$DOMAIN_NAME
+				# 				# LETSENCRYPT_TEST=true
+				#
+				# 				##### Rails
+				# 				# RAILS_MASTER_KEY=testmasterkey
+				# 			EOF
+				# 		fi
+				# 	fi
+				#
+				#
+				# Smartcloud::Apps.start(name)
+				# fi
 			end
 		end
 	end
