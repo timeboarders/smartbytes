25b4daf1bd698ab496e3e7d76965e76cae902dd6
Timeboard <hello@timeboard.me>
2019-08-15 10:20:47 +0530

    added mysql grid

---
+++ lib/smartcloud/grids/mysql.rb
@@ -0,0 +1,60 @@
+# The main Smartcloud Grids Mysql driver
+module Smartcloud
+	module Grids
+		class Mysql
+			def initialize
+			end
+
+			def self.start
+				if Smartcloud::Docker.running?
+					# Creating networks
+					unless system("docker network inspect mysql-network", [:out, :err] => File::NULL)
+						print "-----> Creating network mysql-network ... "
+						if system("docker network create mysql-network", out: File::NULL)
+							puts "done"
+						end
+					end
+
+					# Creating & Starting containers
+					print "-----> Creating container mysql ... "
+					if system("docker create \
+						--name='mysql' \
+						--env MYSQL_RANDOM_ROOT_PASSWORD=yes \
+						--publish='3306:3306' \
+						--volume='#{Smartcloud.config.user_home_path}/.smartcloud/grids/grid-mysql/data:/var/lib/mysql' \
+						--restart='always' \
+						--network='mysql-network' \
+						mysql:5.7.27 \
+						--default-authentication-plugin=mysql_native_password", out: File::NULL)
+
+						puts "done"
+						print "-----> Starting container mysql ... "
+						if system("docker start mysql", out: File::NULL)
+							puts "done"
+						end
+					end
+				end
+			end
+	
+			def self.stop
+				if Smartcloud::Docker.running?
+					# Stopping & Removing containers - in reverse order
+					print "-----> Stopping container mysql ... "
+					if system("docker stop 'mysql'", out: File::NULL)
+						puts "done"
+						print "-----> Removing container mysql ... "
+						if system("docker rm 'mysql'", out: File::NULL)
+							puts "done"
+						end
+					end
+
+					# Removing networks
+					print "-----> Removing network mysql-network ... "
+					if system("docker network rm mysql-network", out: File::NULL)
+						puts "done"
+					end
+				end
+			end			
+		end
+	end
+end
