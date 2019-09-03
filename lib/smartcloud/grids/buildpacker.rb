13dd85dd920b52f9c6fc7d18e065004dc6fda4d5
Timeboard <hello@timeboard.me>
2019-09-03 19:12:50 +0530

    upgraded buildpacker to be a grid

---
+++ lib/smartcloud/grids/buildpacker.rb
@@ -0,0 +1,30 @@
+module Smartcloud
+	module Grids
+		class Buildpacker < Smartcloud::Base
+			def initialize
+			end
+
+			def pack
+				pack_rails if File.exist? "bin/rails"
+				logger.info "Could not continue ... Launch Failed."
+			end
+
+			def pack_rails
+				# Remove server.pid if it exists
+				FileUtils.rm("tmp/pids/server.pid") if File.exist? "tmp/pids/server.pid"
+
+				logger.info "Performing bundle install ..."
+				if system("bundle install --deployment --clean")
+					logger.info "Installing Javascript Dependencies & Pre-compiling Assets ..."
+					if system("bundle exec rails assets:precompile", out: File::NULL)
+						logger.debug "Starting Server ..."
+						if system("god -c Godfile -D")
+							logger.info "Launched Application ... Success."
+						end
+						# exit 0
+					end
+				end
+			end
+		end
+	end
+end
