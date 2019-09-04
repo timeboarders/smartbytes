49e88a7902bc791248ef85bf88cc7bb1d8f8c4f2
Timeboard <hello@timeboard.me>
2019-09-04 19:48:44 +0530

    added logger formatting to buildpacker

--- lib/smartcloud/grids/buildpacker.rb
+++ lib/smartcloud/grids/buildpacker.rb
@@ -5,24 +5,37 @@ module Smartcloud
 			end
 
 			def pack
+				set_logger_formatter_arrow
+
 				pack_rails if File.exist? "bin/rails"
-				logger.info "Could not continue ... Launch Failed."
+
+				logger.error "Could not continue ... Launch Failed."
+				logger.formatter = nil
+				exit 1
 			end
 
 			def pack_rails
 				# Remove server.pid if it exists
 				FileUtils.rm("tmp/pids/server.pid") if File.exist? "tmp/pids/server.pid"
 
-				logger.info "Performing bundle install ..."
-				if system("bundle install --deployment --clean")
-					logger.info "Installing Javascript Dependencies & Pre-compiling Assets ..."
-					if system("bundle exec rails assets:precompile", out: File::NULL)
-						logger.debug "Starting Server ..."
-						if system("god -c Godfile -D")
-							logger.info "Launched Application ... Success."
-						end
-						# exit 0
-					end
+				# if system("god -c Godfile -D")
+					return true
+				# end
+				# return false
+			end
+
+			def set_logger_formatter_arrow
+				logger.formatter = proc do |severity, datetime, progname, message|
+					severity_text = { "DEBUG" => "\u{1f527} #{severity}:", "INFO" => " \u{276f}", "WARN" => "\u{2757} #{severity}:",
+						"ERROR" => "\u{274c} #{severity}:", "FATAL" => "\u{2b55} #{severity}:", "UNKNOWN" => "\u{2753} #{severity}:"
+					}
+					"\t\t\t\t#{severity_text[severity]} #{message}\n"
+				end
+			end
+
+			def set_logger_formatter_tabs
+				logger.formatter = proc do |severity, datetime, progname, message|
+					"\t\t\t\t       #{message}"
 				end
 			end
 		end
