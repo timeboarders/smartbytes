18d2414daed1f30e5aea26fc73d54510be69ed88
Timeboard <hello@timeboard.me>
2019-10-17 16:26:20 +0530

    made .smartcloud as default dir in smartcloud.sh

--- lib/smartcloud/templates/dotsmartcloud/bin/smartcloud.sh
+++ lib/smartcloud/templates/dotsmartcloud/bin/smartcloud.sh
@@ -3,7 +3,6 @@
 docker run -it --rm \
 	-v "/home/$(whoami)/.smartcloud:/home/$(whoami)/.smartcloud" \
 	-v "/var/run/docker.sock:/var/run/docker.sock" \
-	-v "$PWD:$PWD" \
-	-w "$PWD" \
+	-w "/home/$(whoami)/.smartcloud" \
 	-u `id -u` \
 	smartcloud "$@"
