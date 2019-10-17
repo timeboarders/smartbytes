ffffb35b0775dabbf978cf8cbeb5d2c019c92151
Timeboard <hello@timeboard.me>
2019-10-17 10:19:54 +0530

    added engine install support

--- lib/smartcloud/engine.rb
+++ lib/smartcloud/engine.rb
@@ -5,40 +5,31 @@ module Smartcloud
 		end
 
 		def self.install
+			ssh = Smartcloud::SSH.new
+			machine = Smartcloud::Machine.new
+
+			system("mkdir -p ./tmp/engine")
+			system("cp #{Smartcloud.config.root_path}/lib/smartcloud/engine/Dockerfile ./tmp/engine/Dockerfile")
+
+			machine.sync first_sync: true
+
+			puts "-----> Creating image smartcloud ... "
+			ssh.run "docker image build -t smartcloud \
+					--build-arg DOCKER_GID=`getent group docker | cut -d: -f3` \
+					--build-arg USER_UID=`id -u` \
+					--build-arg USER_NAME=`id -un` \
+					~/.smartcloud/tmp/engine"
+
+			puts "-----> Adding smartcloud to PATH ... "
+			ssh.run "chmod +x ~/.smartcloud/bin/smartcloud.sh && sudo ln -sf ~/.smartcloud/bin/smartcloud.sh /usr/local/bin/smartcloud"
+			system("rm ./tmp/engine/Dockerfile")
+
+			machine.sync
 		end
 
 		def self.uninstall
-		end
-
-		def create_images
-			# unless system("docker image inspect smartcloud", [:out, :err] => File::NULL)
-			# 	print "-----> Creating image smartcloud ... "
-			# 	if system("docker image build -t smartcloud \
-			# 		--build-arg DOCKER_GID=`getent group docker | cut -d: -f3` \
-			# 		--build-arg USER_UID=`id -u` \
-			# 		--build-arg USER_NAME=`id -un` \
-			# 		#{Smartcloud.config.root_path}/lib/smartcloud/engine")
-			# 		puts "done"
-			# 	end
-			# end
-
-			#docker build -t smartcloud \
-			#  --build-arg USER_UID=`id -u` \
-			#  --build-arg USER_GID=`id -g` \
-			#  --build-arg USER_NAME=`id -un` \
-			#  .
-
-			# docker run -it --rm -v "/home/$(whoami)/.smartcloud:/home/$(whoami)/.smartcloud" smartcloud
-			#
-			# system("docker create \
-			# 	--name='smartcloud' \
-			# 	--volume='#{Smartcloud.config.user_home_path}/.gem:#{Smartcloud.config.user_home_path}/.gem' \
-			# 	--volume='#{Smartcloud.config.user_home_path}/.smartcloud/config:#{Smartcloud.config.user_home_path}/.smartcloud/config' \
-			# 	--volume='#{Smartcloud.config.user_home_path}/.smartcloud/grids/grid-runner:#{Smartcloud.config.user_home_path}/.smartcloud/grids/grid-runner' \
-			# 	--volume='/var/run/docker.sock:/var/run/docker.sock' \
-			# 	--restart='always' \
-			# 	--network='nginx-network' \
-			# 	smartcloud/runner", out: File::NULL)
+			ssh.run "sudo rm /usr/local/bin/smartcloud"
+			ssh.run "docker rmi smartcloud"
 		end
 	end
 end
