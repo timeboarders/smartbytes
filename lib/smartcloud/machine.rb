19e5d1692ec95a84bf4ef3dda341eded2e38dd88
Timeboard <hello@timeboard.me>
2019-10-22 12:42:51 +0530

    added comments for sync

--- lib/smartcloud/machine.rb
+++ lib/smartcloud/machine.rb
@@ -159,10 +159,12 @@ module Smartcloud
 		private
 
 		def sync_pull
+			puts "-----> Sync pulling ... "
 			system("rsync -azumv --delete --include=*/ --include={#{sync_pull_files_list}} --exclude=* -e ssh #{Smartcloud.credentials.machine[:username]}@#{Smartcloud.credentials.machine[:host]}:~/.smartcloud/ .")
 		end
 
 		def sync_push
+			puts "-----> Sync pushing ... "
 			system("rsync -azumv --delete --include=*/ --include={#{sync_push_files_list}} --exclude={#{excluded_sync_files_list}} --exclude={#{sync_pull_files_list}} -e ssh ./ #{Smartcloud.credentials.machine[:username]}@#{Smartcloud.credentials.machine[:host]}:~/.smartcloud")
 		end
 
