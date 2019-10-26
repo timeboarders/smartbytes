f1d502206e86b042a685716aec03d1dab5574976
Timeboard <hello@timeboard.me>
2019-10-27 01:34:45 +0530

    added apps repositories volume to redmine

--- lib/smartcloud/grids/redmine.rb
+++ lib/smartcloud/grids/redmine.rb
@@ -22,6 +22,7 @@ module Smartcloud
 						--env REDMINE_DB_PASSWORD=#{Smartcloud.credentials.redmine[:database_password]} \
 						--env REDMINE_DB_DATABASE=#{Smartcloud.credentials.redmine[:database_name]} \
 						--env REDMINE_PLUGINS_MIGRATE=#{Smartcloud.credentials.redmine[:plugins_migrate]} \
+						--volume='#{Smartcloud.config.user_home_path}/.smartcloud/apps/repositories:/repositories:ro' \
 						--volume='#{Smartcloud.config.user_home_path}/.smartcloud/grids/grid-redmine/files:/usr/src/redmine/files' \
 						--restart='always' \
 						--network='nginx-network' \
