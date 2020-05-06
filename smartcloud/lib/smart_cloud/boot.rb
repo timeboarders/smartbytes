0961f785b7bce4d46024a37d53747d9404b8ac3f
Timeboard <hello@timeboard.me>
2020-05-06 21:54:09 +0530

    removed unncessary requires in boot.rb for smartcloud and smartmachine

--- smartcloud/lib/smart_cloud/boot.rb
+++ smartcloud/lib/smart_cloud/boot.rb
@@ -17,14 +17,8 @@ require 'smart_cloud/buildpacker'
 
 require 'smart_cloud/user'
 
-require 'smart_cloud/grids/elasticsearch'
-require 'smart_cloud/grids/minio'
-require 'smart_cloud/grids/mysql'
 require 'smart_cloud/grids/nextcloud'
-require 'smart_cloud/grids/nginx'
-require 'smart_cloud/grids/prereceiver'
 require 'smart_cloud/grids/redmine'
-require 'smart_cloud/grids/solr'
 
 require 'smart_cloud/apps/app'
 require 'smart_cloud/apps/rails'
