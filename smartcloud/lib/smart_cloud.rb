aac513ff3fbfd9541caea1a4b5b3ffcc7de727d3
Timeboard <hello@timeboard.me>
2020-05-06 19:57:52 +0530

    renamed smartcloud to smart_cloud in smartcloud/lib folder

---
+++ smartcloud/lib/smart_cloud.rb
@@ -0,0 +1,24 @@
+require "ostruct"
+require "yaml"
+
+# The main SmartCloud driver
+module SmartCloud
+	def self.config
+		@@config ||= OpenStruct.new
+	end
+
+	def self.credentials
+		@@credentials ||= OpenStruct.new(SmartCloud::Credentials.new.config)
+	end
+end
+
+require 'smart_cloud/version'
+
+SmartCloud.config.root_path = File.expand_path('../..', __FILE__)
+SmartCloud.config.user_home_path = File.expand_path('~')
+if File.exist?("#{SmartCloud.config.user_home_path}/.cloud/config/environment.rb")
+	require "#{SmartCloud.config.user_home_path}/.cloud/config/environment"
+end
+
+require 'smart_cloud/base'
+require 'smart_cloud/boot'
