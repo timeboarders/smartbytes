698d23c8ae884112c000458944aa42744361a734
Timeboard <hello@timeboard.me>
2020-05-07 12:14:11 +0530

    updated comment in envrionment template file

--- smartmachine/lib/smart_machine/templates/dotsmartmachine/config/environment.rb
+++ smartmachine/lib/smart_machine/templates/dotsmartmachine/config/environment.rb
@@ -1,6 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
 # => NOTE: Ensure that the specified top-level domains are pointing to this server ip address using DNS records.
+# => Be sure to restart your server when you modify this file.
 
 # Top-level naked domain to be used for subdomains of apps.
 SmartMachine.config.apps_domain = "yourdomain.com"
