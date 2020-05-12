56922175510a1748b3d583a0d58f7502d26344b1
Timeboard <hello@timeboard.me>
2020-05-12 23:15:16 +0530

    changed version to 0.7.0

--- smartcloud/lib/smart_cloud/gem_version.rb
+++ smartcloud/lib/smart_cloud/gem_version.rb
@@ -8,7 +8,7 @@ module SmartCloud
 
   module VERSION
     MAJOR = 0
-    MINOR = 6
+    MINOR = 7
     TINY  = 0
     PRE   = nil
 
--- version.rb
+++ version.rb
@@ -8,7 +8,7 @@ module Smartbytes
 
   module VERSION
     MAJOR = 0
-    MINOR = 6
+    MINOR = 7
     TINY  = 0
     PRE   = nil
 
