a8f4c0a496059b552f54aa6e115fc344d619fa25
Timeboard <hello@timeboard.me>
2020-05-20 15:33:08 +0530

    added mysql scheduling to scheduler

--- smartmachine/lib/smart_machine/grids.rb
+++ smartmachine/lib/smart_machine/grids.rb
@@ -9,6 +9,7 @@ module SmartMachine
 
 			valid_actions = ['up', 'down']
 			valid_actions.push(*['backup', 'flushlogs']) if grid_name == 'mysql'
+			valid_actions.push(*["start", "stop"]) if grid_name == 'scheduler'
 			raise "invalid action on the grid #{grid_name}" unless valid_actions.include? action
 
 			Object.const_get("SmartMachine::Grids::#{grid_name.capitalize}").new.public_send(action, *args)
--- smartmachine/lib/smart_machine/grids/scheduler.rb
+++ smartmachine/lib/smart_machine/grids/scheduler.rb
@@ -61,10 +61,11 @@ module SmartMachine
 						print "-----> Creating container scheduler with image #{scheduler_image_name} ... "
 						if system("docker create \
 							--name='scheduler' \
-							--user `id -u` \
+							--env MAILTO=#{SmartMachine.config.sysadmin_email} \
+							--user `id -u`:`id -g` \
 							--workdir /home/`id -un`/.smartmachine \
 							--volume='#{SmartMachine.config.user_home_path}/.smartmachine/config:#{SmartMachine.config.user_home_path}/.smartmachine/config' \
-							--volume='#{SmartMachine.config.user_home_path}/.smartmachine/grids/scheduler/crontabs:/etc/crontabs' \
+							--volume='#{SmartMachine.config.user_home_path}/.smartmachine/grids/scheduler/crontabs:/crontabs' \
 							--volume='/var/run/docker.sock:/var/run/docker.sock:ro' \
 							--restart='always' \
 							#{scheduler_image_name}", out: File::NULL)
@@ -73,6 +74,10 @@ module SmartMachine
 							print "-----> Starting container scheduler with image #{scheduler_image_name} ... "
 							if system("docker start scheduler", out: File::NULL)
 								puts "done"
+
+								restore_crontabs
+							else
+								puts "error"
 							end
 						end
 					end
@@ -98,26 +103,81 @@ module SmartMachine
 				end
 			end
 
-			def start_mysql_backups
-				print "-----> Starting automatic backup schedule for mysql ... "
-				if system("whenever --set 'MAILTO=#{SmartMachine.config.sysadmin_email}' --load-file #{SmartMachine.config.user_home_path}/.smartmachine/config/mysql/schedule.rb --update-crontab", out: File::NULL)
-					puts "done"
-				else
-					puts "error"
+
+			def start(*args)
+				args.flatten!
+				type = args.empty? ? '--all' : args.shift
+
+				self.class.running!
+
+				if type == '--mysql'
+					system("docker exec -i scheduler sh -c 'exec scheduler start --mysql'")
 				end
 			end
 
-			def stop_mysql_backups
-				print "-----> Stopping automatic backup schedule for mysql ... "
-				if system("whenever --set 'MAILTO=#{SmartMachine.config.sysadmin_email}' --load-file #{SmartMachine.config.user_home_path}/.smartmachine/config/mysql/schedule.rb --clear-crontab", out: File::NULL)
-					puts "done"
-				else
-					puts "error"
+			def stop(*args)
+				args.flatten!
+				type = args.empty? ? '--all' : args.shift
+
+				self.class.running!
+
+				if type == '--mysql'
+					system("docker exec -i scheduler sh -c 'exec scheduler stop --mysql'")
 				end
+			end
+
+			def mysql(*args)
+				args.flatten!
+				action = args.empty? ? '' : args.shift
+
+				if action == 'start'
+					print "-----> Starting automatic backup schedule for mysql ... "
+					if system("whenever --load-file #{SmartMachine.config.user_home_path}/.smartmachine/config/mysql/schedule.rb --update-crontab", out: File::NULL)
+						puts "done"
+					else
+						puts "error"
+					end
+				elsif action == 'stop'
+					print "-----> Stopping automatic backup schedule for mysql ... "
+					if system("whenever --load-file #{SmartMachine.config.user_home_path}/.smartmachine/config/mysql/schedule.rb --clear-crontab", out: File::NULL)
+						puts "done"
+					else
+						puts "error"
+					end
+				end
+			ensure
+				backup_crontabs
 			end
 
 			def scheduler_image_name
 				"smartmachine/scheduler:#{SmartMachine.version}"
+			end
+
+			def restore_crontabs
+				if system("docker exec scheduler sh -c 'exec test -f /crontabs/`id -un`'")
+					print "-----> Restoring latest crontabs ... "
+
+					if system("docker exec scheduler sh -c 'exec crontab - < /crontabs/`id -un`'")
+						puts "done"
+					else
+						puts "error"
+					end
+				end
+			end
+
+			def backup_crontabs
+				print "-----> Backing up latest crontabs ... "
+				if system("crontab -l > /crontabs/`id -un`", out: File::NULL)
+					puts "done"
+				else
+					puts "error"
+				end
+			end
+
+			def self.running!
+				unless system("docker inspect -f '{{.State.Running}}' 'scheduler'", [:out, :err] => File::NULL)
+					raise "Scheduler is not running. Please start scheduler before scheduling"
+				end
 			end
 		end
 	end
