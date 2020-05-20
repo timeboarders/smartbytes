7c04d709f8cb9d6934d7c6f2e64cb286a67f2c2f
Timeboard <hello@timeboard.me>
2020-05-20 17:23:43 +0530

    updated mysql schedule commands

--- smartmachine/lib/smart_machine/templates/dotsmartmachine/config/mysql/schedule.rb
+++ smartmachine/lib/smart_machine/templates/dotsmartmachine/config/mysql/schedule.rb
@@ -3,17 +3,17 @@
 
 # Flush logs used to create consistent binlogs to be used for incremental backups
 every :day, at: '12:00 pm' do
-	command "/usr/bin/smartmachine grids mysql flushlogs"
+	command "smartmachine runner grids mysql flushlogs"
 end
 
 # Create daily backup.
 # This also flushes the logs before backup
 every :day, at: '12:00 am' do
-	command "/usr/bin/smartmachine grids mysql backup --daily"
+	command "smartmachine runner grids mysql backup --daily"
 end
 
 # Promote currently latest daily backup to weekly backup.
 # This is only possible when a daily backup creation has already been completed.
 every :monday, at: '3:00 am' do
-	command "/usr/bin/smartmachine grids mysql backup --promote-to-weekly"
+	command "smartmachine runner grids mysql backup --promote-to-weekly"
 end
